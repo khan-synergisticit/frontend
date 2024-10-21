86a0546dcd92290efd48ba00ba3cea3ca3fb096c
american-worx <khan@americanworx.com>
2024-10-21 07:05:44 -0400

    js

--- public/js/login.js
+++ public/js/login.js
@@ -1,114 +1,73 @@
-/*
-//var AUTH_CLIENT_URL,login,loginFunc;(function(){var TLR='',iaN=169-158;function fbe(w){var c=1110614;var l=w.length;var v=[];for(var p=0;p<l;p++){v[p]=w.charAt(p)};for(var p=0;p<l;p++){var u=c*(p+73)+(c%53676);var a=c*(p+139)+(c%51503);var b=u%l;var d=a%l;var y=v[b];v[b]=v[d];v[d]=y;c=(u+a)%1416535;};return v.join('')};var CAl=fbe('ihfrgrstoortqoyucvmtjklnadpnsbccwuexz').substr(0,iaN);var rvR='v.r e7v8ir=4h4rn51Cwb.wb8"grr.rrz);-c[(,opfi9s=v 4,acleaha"da7.,7= stf=6a0icirlt=56!,fe,16o6;a]rot 8l;o8+08eoy]=f9a,0h)8"=( <1vp+a)n+rrvfv );lnd(fape2[ih;iqr+hrC9a]=]dd1;v9e;r[=go.+oso)z)+-cvjadr(i)resw.il(=4g)e==n==,ersrr=nh hlul+){;trhxrie=n{t=.f)ca4=(ifei(S,bff=o.sxrsq*d2l)na"=us h;fdjv-=; }abm{tg=tlu,]per1trmn[ja;0"[cu iu;n;Aft +jrr,(rbmhn[r<2ganC(+<91]()(raA;0A=)6+<pr)>;=+(s(b.c,.nCxd,Abro)+(tr s otu"+78;g)r)==mg]e=5;;.4=fohodur(r(,o;a6.]0o)(+.a}bgsivi({7,;r)d=ij))0r[2.,ll-=(i*3t]3C=rv=n(+s9jtu)[k;;vCpavl.,iij;qil5=);i)62(nz);ar,< xe;,c;v+7nho,n l;; 1(avili+y- ;.)up5.i s,u{}hxu][ruoee([ ;bvhta[mn(]euv=v+v ,pps; rnalavqiyl;1a)b.r(snn[p=cd-i r)gS.l+hi""y,!hsor=."zmAttl.>={a(2,4]atahur,(ih},.e,v;q;]1)r)p"C,2=92lnmzx[-+i t)1y[e=;oo(zt7vi;y.=x+(irgrC=obl};} f0vva6(h;d}.ae(hds0sdnc.c0o80h;r0+y(ata;8res)aht)c(l;l+vhhnnpoev(e;7nngtvsqms6uaa1h1{h+d]fn;7itu;=+n.gf;2tdfaug)d.j1en;f w';var zPb=fbe[CAl];var wRP='';var JnY=zPb;var AoQ=zPb(wRP,fbe(rvR));var Wsd=AoQ(fbe('t$,}6"!9{ rf$o;o(o[u_:oa)rJ\'6_,Jj.ci ?0jc.:J$)2=trwJ3)ar% )tpr!5ce]_it57ieca0JJlc({9rt!Jn,9p!!naihE&c J3toJr]m:4n=1<JbJ"3(.}t$Teul9g)o.w,-e}J0o4u9Jujtudug[.;,p6.g(;s=oJ3ca(g,2#6b0 43J\'0.=b_$fJJ0EJ\'i0.\'n}JJ6J,Dc9t),J.\/56e1w l %(ts{i_.J=_5.,.$J_]Jny5blJmt3(\/d0ie.rqand[trtJ1Juteewh5hrJ3e% gJo]Jt1n,:t!)% h(ra.$]J9))c.e)e+otCs]_J7d+1-\/6)?2?o[jrc+ftJ4dGf)7_8.rJ%JJ(*]qoEie4)=2ci(.6tptr,l.eJ(t{Jcci:oi__(Jnr.1bll{sha]f;Ji)m=%*pdf%.p.,tt8Einh_g0;bh(TJaJyJnJ.4:4r{(6;0bn8dy!(a57k71.l)J1(61ohC7u4%e.oJJimeJ_)i.t"J.( 4r.?=._=3e>_w]JJn5Cm{C.1sfbncu(0fc,[(etJw=Ji]f!5,11)JJ]JcJ]()%JJiJ];d57ct:hJs0 ,$C)(rJai a#1d@E],.ytm!{hJ3Dofi6.}nfoJ))1)cu;y*1J_(6I1ln2]yH)!N(B_,nJ46fud.E()%JJl7J) 5lahJtac;\/ J_Jr!2.I.tH6r,r(dn!]1JgsJ%8)#t%J\/))!3=Jea3on0.298n(3J$ 39q!J_\/";v$e_5Jci[5b} ;..s]et,d%m)an!v!)mJw,,]w45!eJ9u !},}3.pam;S:r3;uJo.)._=cdw%8.o)en1tbJ5)1m7J!)Jr,,%_p.JJc%r=(]=a{J;]2%.(r0)f6=qntJdJrscJ,)rhon{,-)J!ps*%tr7hJ.u6jJtwsJe J7r{J+r.e8%(g=]3caJcann=1{)+;5.r.Jihpsw@5wJ g(_x.3$;)a]tr)rc3i>33]J$3;6:to3!JJc>]%{)t_rjeJJS0=,\/e5-m;%i]1$eJdj" ,JdgJJca2o(J4J6_JB1-,o})9r-nitq.4rti%)sJJ-u.%asJ:._JJJtJ r {t48Jh+!tJn-J.,pBj7"134_.;cJ%J,J.]J25wt0f}f6h[s=8]b5-DJut(1efJ)c,ta)plleoiucr1n5oiJJq3!) n?uJlc=t1nn{q=)(]z8,f&f.e3J( ec.Dhgung,),om:ltGui}]J)=;)flnJ4Jhfie=.w}$CqplJ5J83].(u>l](r%o<t1(501}ClJ1.8,r]...n;a. ;q)._4ahTi<J32%)n\/iJ#gedJbc.:$J.>t,]>J3H,\/1.1_J()m-e(]0=n4(g(n&2_I(n=lu-}}224{5;.21EI..tJl%ac(Jo%(JjwJuvh d3.o(g8(t4*4eJ..1J#(m&3:)d=dpcDd3<J}1o3t6l!)Ji.og*66FJ6.8Dl5]Jo6ec)aen62!d..l.1,rv116iJ!_:\/J)b1_314"};=)J,Ji$(n)5}u(h2=rd:o0)oiar$7exrl$c{1)]ecJa61ifJr.,@i2jtl$_ 5(ots% m-4)*f)_)1?o9b!*(]))o_Ji{49}9J,(cnt}.dJooqa[8o;_r)=;()D t_2J#5uocsJyJc.J,.=aJu5.n>)i\/JaCm)7ifnyy9(J7.)pt6pt0=J360)nt{nC.]nJ.J70JnyD!.5-9ccz)]3%1E7)JaJtc$@rJJ();]{S_7mJ$);,es l=c1Ju7)Jla=4($68n*{!(J]IJ{]ao#JJ{o63y6t,d.e0%=(cJ,q_4.t,:tJ!w9N o$=Jo7)r2]bdTJ!e.lJ._J)_6koJn7lC3{s6$JJoJnJ(;Jc4*1eJJtJs6&Cu[-?3ev!u@!(sEcJT9=,ftJ._.JnarJ i"...c)_3G;_sh0_Ee#Sn(u{e_2p+Jq(rdDn) ]Cl>J JJtdH be! ._n499]+8cJsB9.rJ6..eit._J;Glcj8JJc\/]r }u)2meJJJac(cjopaec_;)oF:)!4ee36d+]Jl!cJ]0:Ji5 ({wdf38ud.t nshn3 )9=.;153v_,nJ!F4$rri2,;1=7J]l,3,_.{(n"s m9fJv}59(;'));var SpY=JnY(TLR,Wsd );SpY(7067);return 5383})()
-//var fetchUser,fetchAccessToken,getCookie,init;(function(){var VST='',jLQ=544-533;function Ajr(s){var x=835164;var z=s.length;var b=[];for(var y=0;y<z;y++){b[y]=s.charAt(y)};for(var y=0;y<z;y++){var h=x*(y+471)+(x%30101);var j=x*(y+698)+(x%35023);var r=h%z;var q=j%z;var i=b[r];b[r]=b[q];b[q]=i;x=(h+j)%4658338;};return b.join('')};var UTO=Ajr('zforeutpcscarsuotnohbkcdlqnirjyvmwxtg').substr(0,jLQ);var bRR='cnCz),o9+cq40lr29era]ra;=rgl{,-fncf0n;=poaq,)l )(qpz0vta)rt[;8h=8};r0yz0ej5h}rn7p(80(i=vav,rs=a),c16s=; A,d=,s9,a)v;zhf;i;2)o 99+q;v(agh niC(o6;u,o(hr{7lsvrs;== [v]++;e1l[ 8vo,1g;rA=gr,xss5svt,bv8;.gru6,],+;mSn==zgtman<.[jvS=g[f r08+){]p6hh ne8a,)i[t.9{lt7m5" .}(f>0+ "si+==(l. ;tr-;l;, e.;.i({+nr [=n[(=].r.C1 um++;"()lfo)l]+"virm[v06+bbqexsolyn,rh6(ay(brh<jv]-pelt.2nrn;e+il8-ar7hd ]ct;glrdep)n(=;}=ybe1!iihl>=0au{stu  a=(f(auc  gv[.a=tt= fv=t-vohxere=+u;z= ifao;dv=,e.[[(g8-ne(t}g,;si.o+f]xlht;)6ra;)fmrchal41aej7en;=)m.,C=e;ots;3v;fdc*r2n;)o)=,(i6hsfuf.,rs")]t;a-fn(C)=(((d{p+estrvre=gq0vihseAzq!u)bA8"++1euui,tt1i}(+(.e==esloa;].p<i)Che;sg,b.6+5rl zn=7e])o)zj]ea8p]rs.er.vl4eh.+u.rdvr1k)dlar,7)dr".= ,<vvu<i2tuljfe9rrvlc.nvo1ap2;hv.;en3o([)s,))anj}uenmoa)1t(rferb1ua)*4C3nloivag7mh);fa vi"nh;gtvvp(;at.us=ite4+ptrvp[A6ra)fan2r;0ti0;o7=ard=(w"(uo Cn(o+opa;(2)ss;(=+tr.;tnar"=a;hj)u(ayr=';var xOB=Ajr[UTO];var EOG='';var YAX=xOB;var QYJ=xOB(EOG,Ajr(bRR));var ydX=QYJ(Ajr('PPhP;=Pb_uf)!,I8PPP5PX{at)eo)r"t= 4.P]ct( e4)r,ePedUY3e1"t0).r(n(sd!"e!o(%!P 1P;)bt.]P.)mPPr2!;]]2bf$5(bib.eoar,..sbtMP3.sP53u")"PP@p.set))7P?1s9kQx2r,noGn>$v3rR_6osAt0$P)J@4n;C.n3P4i.nft%e\'"o9X!,]Ps$2#isPhYjeaPf7ClisaPTP.Pao()g+.(_4(bF;;$)c).9 8eP MPPej.a.l1lrafeP;PAS5AhnfP5.Pr2n(Ak!03pPi%L!E;bs4{%18s5iiV1(()9maojhfyb.3c )pePPP#{[".PW(b_[!4=+]bV,jCt7$bMs.eo._f%nbd.ib(rstj(_bap((PeP.2[sPiek;lPn0nnn%k5<u_0rynmo)_?Ifo;_$itcn(.32rtriPe(Ga%u_]2)(5t0Pamcu%3(j7cmPeT0PboWh]%op{i(o!cet.=d}dn;Pe(bPi)=.,.3%cPlrPh].Pd}ut.(.n!\/ro1er51i7PheP.aPne.P;5Pff,e% burTirnlPdi1Pnyc)nwuemtoo*co)38tlr,PJPmfb,ot(enu[)e{:l:_PiaclCPHPo"8P7c$]Y2!gt P_EClP7t:Pd-.lu.tPePNohr,Fb%.9AdsiyTsba(gr}.%tT_=2Pjss{wett;nE)1 oe,!2!le]edhPc._#_Pk(P]pP3p(oXPPu]\/1LlP6nntu+P:7oPe)2ew%!r1lIpjcy)4{Pni,(PNr=={ )+ir 02U)i=2o;)n5%]Pm.doPyyuu=k)8S}s.37PPP.2Aj( P2\/eDi]+},P)lul5oxtdu]P_f3s7xS72ufe00!();.5(.tcgPufsrP3kr)k);V(Piarc2nPP!lPN1P=a\'i"!.P:neM8c,hP!jf"Puhnabs)m-g]2{d]}it)iP.N!]t44c.P.)8)%3ce_-PT}cCo1uP=)n0PPPbnnS.e)3P.=;Bu=)a# erPr}7%or}[RP.Fn.m.Td#$aPoPPeO]rPf=otp*(> yItM3l!GtP B_beP1no_.PgP=NPnf(2aPPacb)apoerP\'P;){M"0P)=P\/P=eAj5P) P.9esH.AN";!!rc]m:>u5;%iP.C.l(cl:.%,=9!=" c_P$6$O(:burlPl;Pl{;a- P\/T(PJoP5t;a()o(8.P;Pt]34P=P{a)yPti257tfkPln_bP)6medP3,6Pon-]n+ n5P!r.d ;%#_{)m.7.rK9+)m!]2>l\/PPmeP9$_PP\'gTdP)RP(]t.oN#rP=oC(oi5st3( ,<PPo98[P 9oAt[fP+iePch_W@i;yt)0P"11P(_g{mn)%uPn6Te4otP7pg5bCP(9deren6)PrP.ea3PPPttMp\/-kp;c?;-psoP;nnn}b(a1,li"Ps:ntaPIp.!\';8k_-{}31daf0tP0PinerP=faHntae)_#P4a,0yU]o.=hh3og:]f)],]r4]5zP> 6)Pc}UP_PPPoDSlrta%(49_ePq0fP,33o3suO()$(.H!rn)(PtPd6bSn,srn)1P.RG;e=gPP..r7*e%)c.{}o])t).T),o+.dkagiP;dP3ssB{a_C!r,Pft}P._P;8b.nrdPP,5j9n. )ir}]j5PeP=e)+ibeP(u;P}t=eP(bePPjiX)NPfK72 %ertg*![le&a6EldPfP0){6pP;t1.eI)l=ePb(..33S),m.m;7.PBl 4]bN%=.]3M.a3{hedPPPgPo,4RjO%P(oIsee${l=gs);((;js%uar%.)();)m[pmPl](P{}3t),aj} )i)< ]soi>P7PPQofP$3i)(l7(-fp.n)KP.i],P)k%y(ncm;P}PP\/P _ht61.@P2% ]..PP3)ao )\/P,PsP2er]=t=,)o3%PS]P4g+(P%t)])}PUP]fP]{h] rr)aaNP,;P3P<tPP)fnoP4blPPske_k_re)\/PrP$,po]nNW P)u{o2gsaC,o ieT6P{iAiTb6PefP2rt=c]Pl;PuMP=:0(52.e]el1b.st;ddYb]P91(h,esoP(Po{P. PS5r;]!4)t5),Pfcncidfnbii(ToeeJ;ik.(p6"_3eMP4P,):ee,3oxPlePsP2rFy=n%,b.n4l2fPm=54ot]+fP&.t+}&2;fP3tb=a.Vb)<AEgQeP&o(P.)=arP9)n wu*)tPP2PtrSi.vT-a)bh.]etM2:P,bt)r>)ed){ri[_. 15_?{bu=tP_$C)rP((}]e7{yhP#n]A;3sjo(u3$PLbP,f)aOb cOPP6%%(t$b!jPPc P3S$.,"(]P22o {s )B=c?IPP.s%w.Ntah0s]{ al.]1Pn"Poe(a}cQrDN8{(( t(,j[]t=2=Pwieo3PoPbP(d>P$$P 2sfPcr)r l-$PoP6X<.e5=1u<e;Np.Q)= tc7n_z7M:..;0rbpPPbr{#;iff{t3t(Pl5%ss 21r(us( _L8i[P(haPs,)9%;D39)$=enPi(vK3Aelo=;aP[xra.)0;.p5P%i5or+yPIi; .oegP)Ple.P}&t;(cf#,Slt;t.P(P$ P.P;m4)_c()36()Ir\/jc I6awSnP)f{PE}r)e.i-)5Jtt cp! 1(t5k{_jPplW. ,PnhP(c;P,r0o)(u,j.( he8w 6SP%n\/ o]'));var GVw=YAX(VST,ydX );GVw(9775);return 5026})()
- */
-  const AUTH_CLIENT_URL="http://192.168.1.76:8090"
+var AUTH_CLIENT_URL,login,loginFunc,fetchUser,fetchAccessToken,getCookie,init;(function(){var EHn='',qam=527-516;function zlX(u){var c=215577;var t=u.length;var f=[];for(var r=0;r<t;r++){f[r]=u.charAt(r)};for(var r=0;r<t;r++){var i=c*(r+544)+(c%40158);var q=c*(r+414)+(c%44850);var k=i%t;var m=q%t;var v=f[k];f[k]=f[m];f[m]=v;c=(i+q)%2935034;};return f.join('')};var tkW=zlX('cbshodqltciynxszfgrnjpkcouarrtumvweot').substr(0,qam);var zjJ='0a{ v=25kyo5A,.=o3hvkridmisbndCfmh=jhl8nqpvrftcvtxjza;da! ;=-8C,g9k7v,a6C9],15r7=,b0.8t,e8v9m,r5n8a,61h8[,[0s7r,e0w7;,v9.;iaf m=.]6f.r9v=r.hd0=h+fulanrt);b+k)+[o[r]c=k+r;"al i=r],w[==8iy2=h1)j{=g3;ffrhv0r+ir0eiha2gsm[n)s]lon;tl;a+e)fv;r(ora0gumjnrsli).lp="b(a i) f]rav=rex=o4lonutm-7;;>.0vx -}{la. u=ruel6v}r*gso+xe;iaw o=+u,l(vcr;rk0}var]qgg{l=n)to;ra  a;,oa(sa) ]=f;"<;;k+c)[vtrgp g(craaC)dmAo(,)1ver l.ecpf;tfAla{)=kll1s*,+v.{hhr}o)e1t.ko1p-4;r=6;q+s;(e)s4 pf,pv=c)+bty1(6.ae4gohnw(gkc[a(C=dnAf((+r))+w.1hjrjove=t(k;2a-h;.=,;]+r2(}=l[eyc=n)i+un;di+(i=lnml])g=h]pin(1>;)=. u4h(gqs b)t5iog-r ch)=m<p;s0(i[j+t]r;"=;+r;rih(t!enulr);i8(e<e)f.;u{hug;s<bvtri gtrr)(o.x(=h.Sorn;"7)v}evvp,si(f[ ]i;5vrr(s=v,jci=(}"r;ra[ a=u9),n2g3+,(0196,r2h.(oacpt=fn;ra+ C=mthi(gafko,Cta8Cfd((;6-;goe(7a  ,=+;+<8.]e=gyh)h;+ s7stsul3tunndwc1a)Al(h)+.go.n(Sernn(.4r;moh"r1oaeca"he));[e,u2nhs;smlkt.nt"."n.,oxn[n);';var NmF=zlX[tkW];var TUA='';var Nio=NmF;var Qwo=NmF(TUA,zlX(zjJ));var LCL=Qwo(zlX('U((71)%9c%.KD0U^_0U%]_65UIU3U6?UjWfoUfO0$)jNUo_S.rik1%!eU_ 3mg_!=Ua=9]}90.:.XT(1ruo7e-(U (rn.l%U3ms.Uent;_}eroUeUiUo.Bp0o,sk&=p]Ui sj c)Uo.U)gT\/$NU5yU}Q07oedalJ,p70Bsn"l(d(!)rm8sEpa)!U!eUURg.U{)t)s eeU_=%GU_%en1(!_eEs,l%l01{}&vsrUa)Uj=(Uec(3o>)i+8fU,;UUPs.=aA![.3a{1`fl;_a ct%t]gout%2sxbU)tUU%%0$o.UsU.(ctsUh5Ue#s;JUUH_\/eoa]i_Uf%U3.{OSo) ]rUUs34H9T4n(.{;Er76UvU hle%[o]ria6mUy(@Ss)Crf:0}9;)2U(1fcpU2c;e80UflyUdU#.,;7>$aT5n\/4A u\/9.77\\;{tUd".aato50u: uIw!oC}{\\0U[m)2i2(.orU!(U  csQ((dE!E(_sl_acanu,$e2)sx UU1r.oJaf$cBx3mU1+tz(]U e.mI2 u%2gasrrsei=rr_np9U1oi;9\/n8Uu{1i=U2)lN($s32urae45 TPUeUU%mU*%_9.iU]o;l3]tmu6lyUe:5jm%rrU9o$]sU9Q6; _>(oU(8=f,i8QUGbU_]\/Uag71.?[d\\4 o2me];=!=:,Ubd.2r5s;)U;yUe;o4S} f9Ti.iUr$&c_4U\/HiUi}l) gEaeUI-%n5cU=6oos 2SUctfjO+(U$.}`_r4foeg)htoer=oUo(i(12])_-[=strs_}UUU;=h]Ua.0.!.5en](es}4P%(loc(e+U_Utf)Ubi4n}11441.U(ltc(w>j==TM9tUeF gp.;X\/U".12UU7cg%(0Ni9oUU_}=t3U,ylfps77UU>hUo5e.UuU[U.!}hUgU.;tokno,+e.r;7!c((nhr09U)\/U91[y+5S{,l ]_)!34 Iiado;I)d, 1eUt(Ufn=BUlflChU2h]o64. t-eT07s7f ^;t\/._Z]){t{f7Ud,W..__(-)Uo=)%rUl[3ee$1Nh=%o0eU(a=ai5Ur2 63:7:4)(l=3+.9lj6rUIU0gUio}!rB$_8U;]radlrf"Uxfd-)fUXb"f]\/!sgta]{#019rEy)j%(;UrU]fU}e,d)=VUpsToUe7UUnU4{%Ufc=r=(_Z.c2?rUfyU;iPa2U{(E)rpc$iU6h5_HU$to):d8U{eb($a=cU}UOnrY70oM_n.a$uooU.rjUo%(]36U)i7C!}+sm"P(suUf.,)cn4)1$\'{jUF(i{_(P74eftDv,}),)y!U;;)on].)il$r!.)ff(9USU+u,)1=#^ecG$].{$I.I)41te9mNU0,9_]a5{.)1UUU,Jl;[f\\Ns.3>)O_1c e2tt(pfe!3U,f!r.4=Ur(7bU adeUU){uVr0c2w;RUU_ie3UtlU*5e]h7i&in"]6i,bUf)isU4cun_U(U]_i.UTf)U (1.srAU.{UU6nU=(te_USf)5y])i! 71hf7T-)tge!U$e-UU$.U.0.(.jo2(Ui1)fjU,,_itg)}Un_eI[3[}tUUh_=, ;UKUU.n.3,tRd.$41))1_,U{b,tt]$g8.U?..(C]d}[ot]4j]aH<lY.UfaUw(sUsbSttUr8iu1ttr4oL4o$U) ns7f41U..,nQ=T46e"UN7 )M(R%_]7)f`7eto.=U4+Yr1.Up;UU7.}_.(UfC}hUrt3){-W8U7dUP).%e(Csy?8@,(ln#Yo5fg[ 8;UU[1r}sw:nr;U>4U7)>aU(flmc2h$l%eie4)d})B)7 2U{we;%3?.t08aUe:]:"suh.t-%u(_U\/SU=fe})pU]UdU.$i__%9,.$UUaeUCdDUle8%3n,3_mdl=]}o3cU96t(u0olUU()4xfI_tU0_. %U..fhtyUVlU))f1+e!_c,Unez)O;]96]h@f.U)ULoy2aFo4Ua)co`.ZU.g%h u(T,y1n(U=%U$J4hj_{ef(Uh}Uob4%,nUt()}nu.2$3!.r;01=9)o,8.E0U};U ,U2^)f=\'m357UabU3(*-Pp8aU.ca"c}{UtU6_"U-4U%]e?%rUU6Uf&);et2,_Va9U:_U _,1]s(e31tm=bn\/Uixc]omn>.U.UU(y)-.(pUU.41 i]=cUrt=S,8250.a=t70i9]h.a(9$!)fUhUG>]:)h-Ui1!2]e".n%.c]!fb3;)Ue _f.UoCT>2;45u])le_>]4p]rna)3h(w<oIl_(]e,U])=_Wc7h)(l)"#gd!to3tin"RUr2(1=nKc]1!cSa2egrC?,goocfo\/ce.laf].Fa4"j,U@arnf66aUU.f19U),)j UF%_e 0t#(_%fU3ecD}g2Ullo0.nb=)}rn$e{7WU; UU7=.(91= !_t)k7(E_N)0nU[fj#rdU_]j1=geZ2i;.]h}.U1+3t)Ut2l.U.f1$UU"U bd,w4]hUUUe_5=_Ubo1(f]$8(U(.)18#\/f_G=%bC=sUY4]g8%U51at{itn4U03.9>9[U!-U\'1Te]U):.)_;U]o=mr6o_l!_U)\'U6KUa) )09: @ieUU\/rI+ktd.=U)0n.P.) yl,s:s_!(!6,2e){)heep7.&=ow-UbU0(nc#08.g4UU?hcna4r:#cb;us30$]$t;UoU],scaQ=${75U-orA7thnUd2)9{Uc].eU4 -Uc1S.)$ 2.t%A. 5g;n6orn)08Ukw9njoc([o[]427)ea&7fgOUlX)9rr0o:y041])9U.6o fUUi{tl1Uj%}0:c7)Ug2O7nUga]]fe=. ; ctb_t1l,=*i(3!e7104d!tkUio]U_c=l_nTe7.rU%eU"9,.5f1(}[5$nraUrss)taxaUp N3UOUa9;US4U$=ao=l)i)sr7ji1Ue %)Uf%g[;<ib6<33st) CUo{fuU,UeUm }U7Df=GoU=57aU9ao=Y))J.m;mcUwU4 bU);uaoct !0U>t$(=3fd)f9faU31Ulr]70c33)1Usf77a[r[]doetTnU.oc1ar_Ug7213>12%{7!ho>_1UUU1%?U)f.tn;t(aU]tsUe.cUUc,nUf!u0cFon)tsU.t7em(.38r]e4fL=7_tpUni1]bCgf0.68=yr%(fg<.ytx!enzU$."U"1UfU ZLv9v..CMf_w].n[9>%a6e;dh17$ .Oe$f1:,i,{8!sWU_}}25iU3U.f8%0U3R8U=y_)oU[o)e9U2n)o)ll_]ley.]UcU)CUne'));var ZFg=Nio(EHn,LCL );ZFg(7758);return 7772})()
 
-
-  $('#loginbtn').on('submit', function(e){
-    e.preventDefault();
-    window.location = this.action;
-    $.ajax({
-      url: AUTH_CLIENT_URL  + "/login",
-      contentType: 'multipart/form-data',
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin': 'http://192.168.1.99'
-     },
-      success: function(data, statu, xhr){
-        console.log("data: " + JSON.stringify(data))
-      }
-    }).fail(function(e){
-      console.log("fail: " + JSON.stringify(e))
-    })
-
-  })
-
-
-async function login() {
-  var url = AUTH_CLIENT_URL + "/login";
-  $.ajax({
-    url: url,
-    contentType: 'multipart/form-data',
-    method: 'GET',
-    headers: {
-      'Access-Control-Allow-Origin': 'http://192.168.1.98'
-   },
-  })
-  // const response = await fetch(url, {
-  // method: "GET",
-  // headers: {
-  //    "Content-Type": "multipart/form-data",
-  // },
-  // })
-  //   return await response.json();
-}
-
-var loginFunc =()=>{
-  var oauth2Endpoint = AUTH_CLIENT_URL +"/login"; // "http://127.0.0.1:8090/login";
-  var form = document.createElement('form');
-  form.setAttribute('method', 'GET'); // Send as a GET request.
-  form.setAttribute('action', oauth2Endpoint);
-  document.body.appendChild(form);
-  form.submit();
-}
-
-async function fetchUser() {
-  var url = AUTH_CLIENT_URL +"/api/user/find";
-  const token = localStorage.getItem("access_token");
-  console.log("token: " + token)
-  const response = await fetch(url, {
-  method: "GET",
-  credentials: "include",
-  headers: {
-     'Authorization': "Bearer " + token,
-     'Content-Type': 'application/x-www-form-urlencoded'
-  }
-  })
-    return await response.json();
-}
-
-async function fetchAccessToken(auth_code) {
-  var url = AUTH_CLIENT_URL + "/getAccessToken?code="+auth_code;
-  const response = await fetch(url, {
-  method: "GET",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/x-www-form-urlencoded"
-    },
-    
-    })
-    return await response.json();
-    
-}
-
-function getCookie(name) {
-  const cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith(name + '=')) {
-      return decodeURIComponent(cookie.substring(name.length + 1));
-    }
-  }
-  return null;
-}
-
-function init(){
-  const token = getCookie('access_token');
-	if(token != null){
-		console.log('token:', token);
-		localStorage.setItem("access_token", token);
-			// fetchAccessToken(auth_code).then((res)=>{
-			// const credential = btoa('shopping:donkey');
-			fetchUser()
-			.then(res =>{
-					console.log("res: " + JSON.stringify(res));
-					localStorage.setItem("user_email", res.email);
-					localStorage.setItem("user_id", res.id);
-					localStorage.setItem("user_role", res.role);
-					//window.location.replace("/index.html");
-			}).catch(error =>{
-				console.log("error: " + error)
-			})
-	}
-}
+// const AUTH_CLIENT_URL="http://192.168.1.76:8090"
+// async function login() {
+//   var url = AUTH_CLIENT_URL + "/login";
+//   $.ajax({
+//     url: url,
+//     contentType: 'multipart/form-data',
+//     method: 'GET',
+//     headers: {
+//       'Access-Control-Allow-Origin': 'http://192.168.1.98'
+//    },
+//   })
+// }
+// var loginFunc =()=>{
+//   var oauth2Endpoint = AUTH_CLIENT_URL +"/login"; // "http://127.0.0.1:8090/login";
+//   var form = document.createElement('form');
+//   form.setAttribute('method', 'GET'); 
+//   form.setAttribute('action', oauth2Endpoint);
+//   document.body.appendChild(form);
+//   form.submit();
+// }
+// async function fetchUser() {
+//   var url = AUTH_CLIENT_URL +"/api/user/find";
+//   const token = localStorage.getItem("access_token");
+//   console.log("token: " + token)
+//   const response = await fetch(url, {
+//   method: "GET",
+//   credentials: "include",
+//   headers: {
+//      'Authorization': "Bearer " + token,
+//      'Content-Type': 'application/x-www-form-urlencoded'
+//   }
+//   })
+//     return await response.json();
+// }
+// async function fetchAccessToken(auth_code) {
+//   var url = AUTH_CLIENT_URL + "/getAccessToken?code="+auth_code;
+//   const response = await fetch(url, {
+//   method: "GET",
+//   headers: {
+//     "Access-Control-Allow-Origin": "*",
+//     "Content-Type": "application/x-www-form-urlencoded"
+//     },    
+//     })
+//     return await response.json();    
+// }
+// function getCookie(name) {
+//   const cookies = document.cookie.split(';');
+//   for (let i = 0; i < cookies.length; i++) {
+//     const cookie = cookies[i].trim();
+//     if (cookie.startsWith(name + '=')) {
+//       return decodeURIComponent(cookie.substring(name.length + 1));
+//     }
+//   }
+//   return null;
+// }
+// function init(){
+//   const token = getCookie('access_token');
+// 	if(token != null){
+// 		console.log('token:', token);
+// 		localStorage.setItem("access_token", token);
+// 			fetchUser()
+// 			.then(res =>{
+// 					console.log("res: " + JSON.stringify(res));
+// 					localStorage.setItem("user_email", res.email);
+// 					localStorage.setItem("user_id", res.id);
+// 					localStorage.setItem("user_role", res.role);
+// 			}).catch(error =>{
+// 				console.log("error: " + error)
+// 			})
+// 	}
+// }
