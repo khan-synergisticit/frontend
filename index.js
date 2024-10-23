b42a96fb05b70733fed75409611a73a0b1d96607
american-worx <khan@americanworx.com>
2024-10-23 10:25:33 -0400

    node js

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const router = express.Router();
+const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
-const port = 8080;
+
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Shopping app listening on port 8080!')
+})
