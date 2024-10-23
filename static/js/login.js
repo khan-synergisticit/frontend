8d60fc0230a6431a003cce92d887e13aec4a316d
american-worx <khan@americanworx.com>
2024-10-23 16:04:31 -0400

    login

--- static/js/login.js
+++ static/js/login.js
@@ -62,6 +62,7 @@ async function fetchAccessToken(auth_code) {
 }
 function getCookie(name) {
   const cookies = document.cookie.split(';');
+  console.log(cookies)
   for (let i = 0; i < cookies.length; i++) {
     const cookie = cookies[i].trim();
     if (cookie.startsWith(name + '=')) {
