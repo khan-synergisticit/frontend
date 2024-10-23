c78f08c24d86410809fc4333d9ff079f7f0ca45b
american-worx <khan@americanworx.com>
2024-10-23 10:18:28 -0400

    node js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const router = express.Router();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-  res.sendFile('index.html');
-});
 const port = 8080;
