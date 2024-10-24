3812a7813ba50e196066f6d6ad91270e611f1d6e
american-worx <khan@americanworx.com>
2024-10-24 05:50:01 -0400

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
  //path module of node framework
 // HtmlWebpackPlugin = require('html-webpack-plugin'),
 import HtmlWebpackPlugin from 'html-webpack-plugin';
