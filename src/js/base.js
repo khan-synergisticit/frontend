0ec22537bdd74e4560b551d581a2cb007c3ddb8f
american-worx <khan@americanworx.com>
2024-10-24 13:26:46 -0400

    axios

--- src/js/base.js
+++ src/js/base.js
@@ -7,7 +7,7 @@ $(function(){
 			$("#login-link").append('<button onclick="loginFunc()"  type="button"  class="btn btn-link" style="color: #ffffff;"><i class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i> LOGIN</button>')
 		}else  {
 
-			$("#login-link").append('<button onclick="lougout()"  type="button"  class="btn btn-link" style="color: #ffffff;"><i	class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i> LOGOUT</button>')
+			$("#login-link").append('<button onclick="logout()"  type="button"  class="btn btn-link" style="color: #ffffff;"><i	class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i> LOGOUT</button>')
 		
 			if(role === "ROLE_ADMIN"){
 			$("#role-link").append('<a class="nav-link active" aria-current="page" href="/admin/index.html"><iclass="fa-solid fa-house"></i> Home</a>')
