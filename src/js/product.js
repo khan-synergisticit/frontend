94c75d4ac206ce1e40c32a5265cb11361a83d8e1
american-worx <khan@americanworx.com>
2024-10-24 15:24:10 -0400

    axios

--- src/js/product.js
+++ src/js/product.js
@@ -1,3 +1,21 @@
+$(function(){
+  $.ajax({
+    url: "http://192.168.1.235:8060/api/product/all",
+    type: "GET",
+    success: function(data, status, xhr){
+      console.log("Data: " + JSON.stringify(data))
+      console.log("status: " + JSON.stringify(status))
+      console.log("xhr: " + JSON.stringify(xhr))
+    }
+  }).fail(function(error){
+    console.log("err: " + JSON.stringify(error))
+  })
+})
+
+
+
+
+
 async function fetchProducts() {
   var url = "http://192.168.1.235:8060/api/product/all";
   
