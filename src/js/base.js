59cb7442ed86106d6a14256a1cb22f9d219a83e6
american-worx <khan@americanworx.com>
2024-10-24 13:25:53 -0400

    axios

---
+++ src/js/base.js
@@ -0,0 +1,18 @@
+$(function(){
+  const role = localStorage.getItem("user_role");
+		console.log("role: " + role);
+		
+
+		if(role === null){
+			$("#login-link").append('<button onclick="loginFunc()"  type="button"  class="btn btn-link" style="color: #ffffff;"><i class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i> LOGIN</button>')
+		}else  {
+
+			$("#login-link").append('<button onclick="lougout()"  type="button"  class="btn btn-link" style="color: #ffffff;"><i	class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i> LOGOUT</button>')
+		
+			if(role === "ROLE_ADMIN"){
+			$("#role-link").append('<a class="nav-link active" aria-current="page" href="/admin/index.html"><iclass="fa-solid fa-house"></i> Home</a>')
+		} else {
+			$("#role-link").append('<a class="nav-link active" aria-current="page" href="/index.html"><iclass="fa-solid fa-house"></i> Home</a>')
+		}
+		}
+})
