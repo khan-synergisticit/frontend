197119e1c203dae7f0e1923f6d4fb468e6197043
american-worx <khan@americanworx.com>
2024-10-25 12:48:37 -0400

    category

--- src/js/login.js
+++ src/js/login.js
@@ -35,11 +35,18 @@ async function login() {
 }
 var loginFunc =()=>{
   var oauth2Endpoint = "http://192.168.1.76:8090/login"; // "http://127.0.0.1:8090/login";
-  var form = document.createElement('form');
-  form.setAttribute('method', 'GET'); 
-  form.setAttribute('action', oauth2Endpoint);
-  document.body.appendChild(form);
-  form.submit();
+  // var form = document.createElement('form');
+  // form.setAttribute('method', 'GET'); 
+  // form.setAttribute('action', oauth2Endpoint);
+  // document.body.appendChild(form);
+  // form.submit();
+  $.ajaxForm({
+    url : oauth2Endpoint, // or whatever
+    type: "GET",
+    success : function (response) {
+        alert("The server says: " + response);
+    }
+})
 }
 async function fetchUser() {
   var url = "http://192.168.1.76:8090/api/user/find";
