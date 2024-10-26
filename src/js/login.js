049cba8add9e07a2f8a79e56e54817167f9df0eb
american-worx <khan@americanworx.com>
2024-10-26 10:17:25 -0400

    category

--- src/js/login.js
+++ src/js/login.js
@@ -86,7 +86,7 @@ function init(){
   .then(response => response.json())
   .then(data => {
     const code = data.code;
-    console.log(data.code); // 'Hello from Express!'
+    console.log(data); // 'Hello from Express!'
     if(code != null){
 		
       localStorage.setItem("access_token", code);
