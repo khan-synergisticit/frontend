bf38a7b3ad9c59bcdbe240b92e4fbe05add7290c
american-worx <khan@americanworx.com>
2024-10-26 13:11:12 -0400

    category

--- src/js/axios-service.js
+++ src/js/axios-service.js
@@ -5,24 +5,24 @@ const axiosInstance = axios.create({
   timeout: 1000,
 })
 
-axiosInstance.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  console.log("request: " + JSON.stringify(config))
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-})
-axiosInstance.interceptors.response.use(function (response) {
-  console.log("axios response: " + JSON.stringify(response))
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  return response;
-}, function (error) {
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  return Promise.reject(error);
-});
+// axiosInstance.interceptors.request.use(function (config) {
+//   // Do something before request is sent
+//   console.log("request: " + JSON.stringify(config))
+//   return config;
+// }, function (error) {
+//   // Do something with request error
+//   return Promise.reject(error);
+// })
+// axiosInstance.interceptors.response.use(function (response) {
+//   console.log("axios response: " + JSON.stringify(response))
+//   // Any status code that lie within the range of 2xx cause this function to trigger
+//   // Do something with response data
+//   return response;
+// }, function (error) {
+//   // Any status codes that falls outside the range of 2xx cause this function to trigger
+//   // Do something with response error
+//   return Promise.reject(error);
+// });
 
 export {
   axiosInstance
