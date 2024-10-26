69f4acde9abf882b6da61b586ddd2559d591a1ab
american-worx <khan@americanworx.com>
2024-10-26 13:00:40 -0400

    category

--- src/js/axios-service.js
+++ src/js/axios-service.js
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   timeout: 1000,
 })
 
-axios.interceptors.request.use(function (config) {
+axiosInstance.interceptors.request.use(function (config) {
   // Do something before request is sent
   console.log("request: " + JSON.stringify(config))
   return config;
@@ -13,7 +13,7 @@ axios.interceptors.request.use(function (config) {
   // Do something with request error
   return Promise.reject(error);
 })
-axios.interceptors.response.use(function (response) {
+axiosInstance.interceptors.response.use(function (response) {
   console.log("axios response: " + JSON.stringify(response))
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
